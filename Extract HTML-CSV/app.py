9fe4acef1a9bd09b1495212764490b06240dbd6a
ZKDev01 <rdromaguera2001@gmail.com>
2024-05-10 10:35:20 -0400

    add script: Extract HTML-CSV

---
+++ Extract HTML-CSV/app.py
@@ -0,0 +1,44 @@
+import os
+import fnmatch
+import pandas as pd 
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+url = 'https://cphof.org/standings/icpc/2019'
+
+def extract_tables_from_html(url: str):
+  response = requests.get(url)
+  if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    tables = soup.find_all('table')
+    tables = [table for table in tables]
+    html_files = []
+    for i, table in enumerate(tables, start=1):
+      file_name = 'index_{}.html'.format(i)
+      with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(table.prettify())
+      html_files.append(file_name)
+  else:
+    print("Error al acceder a {}: {}".format(url, response.status_code))
+
+def convert_csv_from_html(dir: str):
+  for dirpath, dirs, files in os.walk(dir):
+    for filename in fnmatch.filter(files, '*.html'):
+      full = os.path.join(dirpath, filename)
+      with open(full, 'r', encoding='utf-8') as f:
+        html = f.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        tables = soup.find_all('table')
+        name_csv = os.path.splitext(filename)[0] + '.csv'
+        with open(name_csv, 'w', newline='', encoding='utf-8') as f_csv:
+          writer = csv.writer(f_csv)
+          for table in tables:
+            rows = table.find_all('tr')
+            for row in rows:
+              columns = row.find_all('td')
+              if columns:
+                writer.writerow([c.text.split() for c in columns])
+
+dir = os.getcwd()
+convert_csv_from_html(dir)
