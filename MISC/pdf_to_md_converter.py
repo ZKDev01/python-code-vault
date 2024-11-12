df94b4a955faa10db91ca7623696463a29f9a39c
ZKDev01 <rdromaguera2001@gmail.com>
2024-11-12 13:08:04 -0500

    fix pdf2md script

--- MISC/pdf_to_md_converter.py
+++ MISC/pdf_to_md_converter.py
@@ -1,42 +1,66 @@
 import os
+import io 
+import base64
 import PyPDF2
+import streamlit as st 
+from PIL import Image
 
-def convert_pdf_to_txt(pdf_path, txt_path):
-  """
+
+PATH = os.getcwd() + '/_data/'
+DOCUMENT = 'experimental-functions.pdf' 
+
+
+def pdf2md():
+  try:
+
+    with open (PATH + DOCUMENT, 'rb') as file:
+      reader = PyPDF2.PdfReader(file)
+      text = dict()
+      images = [ ]
+
+      for i, page in enumerate( reader.pages ):
+        text[i] = page.extract_text()
+        tmp = """
   
-  """
-  with open(pdf_path, 'rb') as file:
-    reader = PyPDF2.PdfReader(file)
-    with open(txt_path, 'w', encoding='utf-8') as output:
-      for page in reader.pages:
-        text = page.extract_text()
-        output.write(text)
+        for image in page.images:
+          image_data = io.BytesIO(image.data)
 
-def convert_pdf_to_md(pdf_path, md_path):
-  """
+          image_base64 = base64.b64encode( image_data.getvalue() ).decode('utf-8')
+          
+          tmp_image_path = f'tmp_{len(images)+1}.png'
+          with open(tmp_image_path, 'wb') as tmp_file:
+            tmp_file.write (image_data.getvalue())
+
+          image_path = f"![]({tmp_image_path})"
+          images.append( image_path )
+
+  Returns:
+      _type_: _description_
+        """
+    
+      return text, images
+
+  except Exception as e:
+    st.write ("fail to converter pdf file")
+    with st.expander( "Error", expanded=False ):
+      st.write (e)
+
+
+
+def main ( ) -> None:
+  st.title ("pdf2markdown Converter")
+
+  markdown, images = pdf2md()
+  st.markdown ( PATH+DOCUMENT )
+
+  for i, page in markdown.items():
+    st.write (f'### Page {i}')
+    st.write (page)
+  # TODO mostrar el pdf original 
   
-  """
-  with open(pdf_path, 'rb') as file:
-    reader = PyPDF2.PdfReader(file)
-    text = ""
-    for page in reader.pages:
-      text += page.extract_text()
-  lines = text.split('\n')
-  md_text = ""
-  for line in lines: 
-    line = line.strip()
-    if line == "":
-      md_text += "\n\n"
-    else:
-      md_text += line + "\n"
-  with open(md_path, 'w', encoding='utf-8') as output:
-    output.write(md_text)
+  # TODO mostrar el markdown convertido 
 
-dir = os.getcwd() 
 
-# Examples
-dir_pdf = 'database/publicacion.pdf'
-dir_txt = 'database/publicacion.txt'
-dir_md  = 'database/publicacion.md' 
 
-convert_pdf_to_txt(dir_pdf, dir_md)
+if __name__ == '__main__':
+  main ( )
