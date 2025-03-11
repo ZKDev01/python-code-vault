86fc7407a269c171e1c39f3da04a3cd71d1fc82f
Daniel <rdromaguera2001@gmail.com>
2025-03-11 04:43:16 -0400

    add algorithm memo-fibonacci

--- algorithms.py
+++ algorithms.py
@@ -81,15 +81,20 @@ def merge (l1: list[int], l2: list[int]) -> list[int]:
   return l
 
 
+def memo_fibonacci(n,memorization) -> None:
+  i = 0
+  n = n+1
+  f = [0]*n
+  f[0]=f[1]=1
+  for i in range(2,n):
+    f[i] = f[i-1] + f[i-2]
+  return f
+
 
 
 def main() -> None:
-  array = [ i**2 for i in range(100) ]
-  target = 9
-
-  result = binary_search (array, target)
-  print(result)
-  
+  f = memo_fibonacci(10,[])
+  print(f)
 
 if __name__ == "__main__":
   main()
